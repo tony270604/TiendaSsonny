909c96fa13e9cf2e119daa263035782dc2456b59
lesssC <leslie.cabrera.luey@gmail.com>
2025-05-20 14:56:08 -0500

    Carga Inicial

---
+++ src/main/java/com/backendWL/BackendWlApplication.java
@@ -0,0 +1,13 @@
+package com.backendWL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendWL/BackendWlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendWL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
