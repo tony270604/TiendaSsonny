e2fcc3d703c1d4ba775ef7a1a971398606d5381e
Prueba <tony270604@gmail.com>
2025-06-14 19:47:25 -0500

    conexion a la base de datos

--- src/confi/VerificarConexion.java
+++ src/confi/VerificarConexion.java
@@ -3,7 +3,7 @@ package confi;
 public class VerificarConexion {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexion.getConexion();
     }
     
 }
